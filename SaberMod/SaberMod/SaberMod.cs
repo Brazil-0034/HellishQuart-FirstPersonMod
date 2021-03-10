f51ab97d47873b25528dda0361195bb8bd709f6a
Admin Man <dynamicpeach48@gmail.com>
2021-03-09 19:47:28 -0800

    add sabermod

---
+++ SaberMod/SaberMod/SaberMod.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using UnityEngine;
+
+namespace SaberMod
+{
+    public class SaberColorChange : MelonMod
+    {
+        /*
+        Mesh swordReplacement;
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            swordReplacement = Resources.Load<Mesh>("LongswordWloszka");
+        }
+        */
+
+        public override void OnUpdate()
+        {
+            if (GameObject.Find("LongswordWloszka"))
+            {
+                GameObject LongswordWloszka = GameObject.Find("LongswordWloszka");
+                LongswordWloszka.transform.Rotate(0, 90 * Time.deltaTime, 0);
+
+                LongswordWloszka.GetComponent<MeshRenderer>().materials[2].SetColor("_Color", new Color(0, 25, 0, 1));
+                LongswordWloszka.GetComponent<MeshRenderer>().materials[2].EnableKeyword("_EMISSION");
+                LongswordWloszka.GetComponent<MeshRenderer>().materials[2].SetColor("_EmissionColor", new Color(1, 2, 1, 1));
+            }
+
+
+            if (GameObject.Find("Kilij3"))
+            {
+                GameObject KilijBase = GameObject.Find("Kilij3");
+                
+                GameObject Kilij3 = KilijBase;
+
+                /*
+                GameObject.Destroy(KilijBase.GetComponent<MeshRenderer>());
+
+                Kilij3.transform.position = KilijBase.transform.position;
+
+                Kilij3.GetComponent<MeshFilter>().mesh = swordReplacement;
+                */
+
+                Kilij3.transform.Rotate(0, 90 * Time.deltaTime, 0);
+
+                Kilij3.GetComponent<MeshRenderer>().materials[0].SetColor("_Color", new Color(25 + Time.deltaTime, 0, 0, 1));
+                Kilij3.GetComponent<MeshRenderer>().materials[0].EnableKeyword("_EMISSION");
+                Kilij3.GetComponent<MeshRenderer>().materials[0].SetColor("_EmissionColor", new Color(1, 2, 1, 1));
+            }
+
+
+            if (GameObject.Find("blade"))
+            {
+                GameObject blade = GameObject.Find("blade");
+                blade.transform.Rotate(0, 90 * Time.deltaTime, 0);
+
+                blade.GetComponent<MeshRenderer>().materials[0].SetColor("_Color", new Color(35 + Time.deltaTime, 0, 0, 1));
+                blade.GetComponent<MeshRenderer>().materials[0].EnableKeyword("_EMISSION");
+                blade.GetComponent<MeshRenderer>().materials[0].SetColor("_EmissionColor", new Color(5, 1, 1, 1));
+            }
+
+
+            if (GameObject.Find("Dragonka"))
+            {
+                GameObject Dragonka = GameObject.Find("Dragonka");
+                Dragonka.transform.Rotate(0, 90 * Time.deltaTime, 0);
+                Vector3 s = Dragonka.transform.localScale;
+                Dragonka.transform.localScale = new Vector3(s.x, s.y, 0.45f);
+
+                Dragonka.GetComponent<MeshRenderer>().materials[0].SetColor("_Color", new Color(45 + Time.deltaTime, 0, 0, 1));
+                Dragonka.GetComponent<MeshRenderer>().materials[0].EnableKeyword("_EMISSION");
+                Dragonka.GetComponent<MeshRenderer>().materials[0].SetColor("_EmissionColor", new Color(1, 2, 1, 1));
+            }
+        }
+    }
+}
